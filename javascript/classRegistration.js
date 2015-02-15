e3e594ebc8c90c02e3226ca8be217dc63d67234e
Noah Teshu <nteshu@gmail.com>
2015-02-14 20:05:05 -0500

    added display of signup form for classes

--- javascript/classRegistration.js
+++ javascript/classRegistration.js
@@ -7,8 +7,24 @@ query.find({success:function(response){
 	classesInList = [];
 	for (var i = 0; i < response.length; i++) {
 		classesInList.push(response[i]);
-		$("#classList").append("<div date-index='"+ i +"'><h2>" + response[i].attributes.title +"</h2></div>");
+		$("#classList").append("<div class='individualClass' date-index='"+ i +"'><h2>" + response[i].attributes.title +"</h2></div>");
 	};
+
+	$(".individualClass").click(function(){
+		$(this).append('<h1>Sign Up</h1>' + 
+			 '<form id="classSignup">' +
+				'<input class="form-control" type="text" placeholder="First Name">' +
+				'<input class="form-control" type="text" placeholder="Last Name">' +
+				'<input class="form-control" type="text" placeholder="Email">' +
+				'<select class="form-control" name="pickSession">' +
+					'<option value="1">2/18/15</option>' +
+					'<option value="2">2/28/15</option>' +
+					'<option value="3">3/10/15</option>' +
+					'<option value="4">3/17/15</option>' +
+				'</select>' +
+				'<button type="submit">Submit</button>' +
+			"</form> ");
+	});
 
 
 }, error: function(){
