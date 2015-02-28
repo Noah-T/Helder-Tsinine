9ad851c225f691ff7d8de181a4ea262139d00141
Noah Teshu <nteshu@gmail.com>
2015-02-28 17:11:32 -0500

    added display of stripe payment form

--- javascript/classRegistration.js
+++ javascript/classRegistration.js
@@ -167,6 +167,74 @@ $("#signUp").click(function(e){
               success: function(){
                 console.log("new user created");
                 $("#createAccount").hide();
+                //display payment form
+                jQuery(function($) {
+  $('#payment-form').submit(function(event) {
+    var $form = $(this);
+
+    // Disable the submit button to prevent repeated clicks
+    $form.find('button').prop('disabled', true);
+
+    Stripe.card.createToken($form, stripeResponseHandler);
+
+    // Prevent the form from submitting with the default action
+    return false;
+  });
+});
+
+function stripeResponseHandler(status, response) {
+  var $form = $('#payment-form');
+
+  
+  //stripe
+  if (response.error) {
+    // Show the errors on the form
+    $form.find('.payment-errors').text(response.error.message);
+    $form.find('button').prop('disabled', false);
+    debugger;
+  } else {
+    console.log(response);
+    debugger;
+    // response contains id and card, which contains additional card details
+    var token = response.id;
+    // Insert the token into the form so it gets submitted to the server
+    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+    // and submit
+    $form.get(0).submit();
+  }
+};
+
+
+$("#formColumn").append(
+              '<form action="" method="POST" id="payment-form">' +
+  '<span class="payment-errors"></span>' +
+
+  '<div class="form-row">' +
+    '<label>' +
+      '<span>Card Number</span>' +
+      '<input type="text" size="20" data-stripe="number"/>' +
+    '</label>' +
+  '</div>' +
+
+  '<div class="form-row">' +
+    '<label>' +
+      '<span>CVC</span>' +
+      '<input type="text" size="4" data-stripe="cvc"/>' +
+    '</label>' +
+  '</div>' +
+
+  '<div class="form-row">' +
+    '<label>' +
+      '<span>Expiration (MM/YYYY)</span>' +
+      '<input type="text" size="2" data-stripe="exp-month"/>' +
+    '</label>' +
+    '<span> / </span>' +
+    '<input type="text" size="4" data-stripe="exp-year"/>' +
+  '</div>' +
+
+  '<button type="submit">Submit Payment</button>' +
+'</form>');
+
               },
               error: function(error){
                 //oops
@@ -195,69 +263,3 @@ $("#signUp").click(function(e){
 
 
 
-// jQuery(function($) {
-//   $('#payment-form').submit(function(event) {
-//     var $form = $(this);
-
-//     // Disable the submit button to prevent repeated clicks
-//     $form.find('button').prop('disabled', true);
-
-//     Stripe.card.createToken($form, stripeResponseHandler);
-
-//     // Prevent the form from submitting with the default action
-//     return false;
-//   });
-// });
-
-// function stripeResponseHandler(status, response) {
-//   var $form = $('#payment-form');
-
-  
-//   //stripe
-//   if (response.error) {
-//     // Show the errors on the form
-//     $form.find('.payment-errors').text(response.error.message);
-//     $form.find('button').prop('disabled', false);
-//     debugger;
-//   } else {
-//     console.log(response);
-//     debugger;
-//     // response contains id and card, which contains additional card details
-//     var token = response.id;
-//     // Insert the token into the form so it gets submitted to the server
-//     $form.append($('<input type="hidden" name="stripeToken" />').val(token));
-//     // and submit
-//     $form.get(0).submit();
-//   }
-// };
-
-//payment form
-// $("#formColumn").append(
-//               '<form action="" method="POST" id="payment-form">' +
-//   '<span class="payment-errors"></span>' +
-
-//   '<div class="form-row">' +
-//     '<label>' +
-//       '<span>Card Number</span>' +
-//       '<input type="text" size="20" data-stripe="number"/>' +
-//     '</label>' +
-//   '</div>' +
-
-//   '<div class="form-row">' +
-//     '<label>' +
-//       '<span>CVC</span>' +
-//       '<input type="text" size="4" data-stripe="cvc"/>' +
-//     '</label>' +
-//   '</div>' +
-
-//   '<div class="form-row">' +
-//     '<label>' +
-//       '<span>Expiration (MM/YYYY)</span>' +
-//       '<input type="text" size="2" data-stripe="exp-month"/>' +
-//     '</label>' +
-//     '<span> / </span>' +
-//     '<input type="text" size="4" data-stripe="exp-year"/>' +
-//   '</div>' +
-
-//   '<button type="submit">Submit Payment</button>' +
-// '</form>');
